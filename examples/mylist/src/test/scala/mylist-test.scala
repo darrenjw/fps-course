c3d42a30225d87dc4319a9926ecc828359fd48aa
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-11-19 10:34:20 +0000

    some unit tests

--- examples/mylist/src/test/scala/mylist-test.scala
+++ examples/mylist/src/test/scala/mylist-test.scala
@@ -4,7 +4,7 @@ import org.scalatest.matchers.should.Matchers
 
 class MyListSpec extends AnyFlatSpec with Matchers {
 
-val smallList = MyList.intList(1,15)
+  val smallList = MyList.intList(15)
 
  "A MyList" should "combine" in {
    val l = (1 :: 2 :: MyNil) ++ (3 :: 4 :: MyNil)
@@ -12,7 +12,7 @@ val smallList = MyList.intList(1,15)
   }
 
   it should "combine sequentially" in {
-    MyList.intList(1,10) ++ MyList.intList(11,20) shouldBe MyList.intList(1,20)
+    MyList.intList(10) ++ MyList.intList(20,11) shouldBe MyList.intList(20)
   }
 
   it should "reverse" in {
@@ -27,6 +27,22 @@ val smallList = MyList.intList(1,15)
     (1 :: 2 :: 3 :: MyNil) map (_*2) shouldBe 2 :: 4 :: 6 :: MyNil
   }
 
+  it should "foldLeft" in {
+    (1 :: 2 :: 3 :: MyNil).foldLeft(0)(_+_) shouldBe 6
+  }
+
+  it should "foldRight" in {
+    (1 :: 2 :: 3 :: MyNil).foldRight(0)(_+_) shouldBe 6
+  }
+
+  it should "foldLeft (non-assoc)" in {
+    (1 :: 2 :: 3 :: MyNil).foldLeft(0)(_-_) shouldBe -6
+  }
+
+  it should "foldRight (non-assoc)" in {
+    (1 :: 2 :: 3 :: MyNil).foldRight(0)(_-_) shouldBe 2
+  }
+
   it should "flatMap" in {
     (1 :: 2 :: 3 :: MyNil) flatMap (a => (a :: a*2 :: MyNil)) shouldBe (
       1 :: 2 :: 2 :: 4 :: 3 :: 6 :: MyNil)
@@ -36,8 +52,17 @@ val smallList = MyList.intList(1,15)
     (2 :: 5 :: MyNil) coflatMap(_.foldLeft(0)(_+_)) shouldBe (7 :: 5 :: MyNil)
   }
 
+  it should "coflatten" in {
+    (2 :: 5 :: MyNil).coflatten shouldBe (2 :: 5 :: MyNil) :: (5 :: MyNil) :: MyNil
+  }
 
-  // Some more based on examples from "main"
+  it should "coflatten.flatten" in {
+    (2 :: 5 :: MyNil).coflatten.flatten shouldBe 2 :: 5 :: 5 :: MyNil
+  }
+
+  it should "flatten a flat list" in {
+    smallList.flatten shouldBe smallList
+  }
 
 
 }
