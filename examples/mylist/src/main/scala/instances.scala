851121079b85d331f2172a4a0546503411e605dc
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2021-11-28 14:19:56 +0000

    updated instances to scala 3 syntax

--- examples/mylist/src/main/scala/instances.scala
+++ examples/mylist/src/main/scala/instances.scala
@@ -6,48 +6,42 @@ Cats instances for MyList
  */
 
 
-object Instances {
+object Instances:
 
-  import cats._
-  import cats.implicits._
+  import cats.*
+  import cats.implicits.*
 
   // MonoidK instance
   // This is how to define a monoid instance for a parameterised type:
   // Uses <+> rather than |+| for combine syntax
-  implicit val monoidMyList: MonoidK[MyList] = new MonoidK[MyList] {
+  given MonoidK[MyList] with
     def combineK[A](a1: MyList[A], a2: MyList[A]): MyList[A] = a1 ++ a2
     def empty[A]: MyList[A] = MyNil
-  }
 
   // Functor instance
-  implicit val functorMyList: Functor[MyList] = new Functor[MyList] {
+  given Functor[MyList] with
     def map[A, B](fa: MyList[A])(f: A => B): MyList[B] = fa.map(f)
-  }
 
   // CoflatMap instance
-  implicit val coflatMapMyList: CoflatMap[MyList] = new CoflatMap[MyList] {
+  given CoflatMap[MyList] with
     def map[A, B](fa: MyList[A])(f: A => B): MyList[B] = fa.map(f)
     def coflatMap[A, B](fa: MyList[A])(f: MyList[A] => B): MyList[B] = fa.coflatMap(f)
-  }
 
   // Monad instance
-  implicit val monadMyList: Monad[MyList] = new Monad[MyList] {
+  given Monad[MyList] with
     def pure[A](a: A): MyList[A] = a :: MyNil
     def flatMap[A,B](fa: MyList[A])(f: A => MyList[B]): MyList[B] = fa.flatMap(f)
     def tailRecM[A, B](a: A)(f: A => MyList[Either[A,B]]): MyList[B] = ???
-  }
 
   // Foldable instance
-  implicit val foldableMyList: Foldable[MyList] = new Foldable[MyList] {
+  given Foldable[MyList] with
     def foldLeft[A, B](fa: MyList[A], b: B)(f: (B, A) => B): B = fa.foldLeft(b)(f)
     def foldRight[A, B](fa: MyList[A],lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
       fa.foldRight(lb)((a: A, eb: Eval[B]) => f(a, eb))
-  }
 
 
 
 
-}
 
 // eof
 
